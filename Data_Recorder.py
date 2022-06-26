3c2caf01a8095cd66a9223b2b848777a01c4bfba
dht2003 <44319637+dht2003@users.noreply.github.com>
2022-06-26 14:57:09 +0300

    added cut to footage recorder

--- Data_Recorder.py
+++ Data_Recorder.py
@@ -13,7 +13,7 @@ class DataRecorder(tk.Tk):
         super(DataRecorder, self).__init__()
 
         self.title('Data Recorder')
-        self.geometry('500x200')
+        self.geometry('500x300')
         self.controller_state = ControllerState()
         self.Mk_screen_capture = MKScreenCapture()
         self.fourcc = cv2.VideoWriter_fourcc(*"XVID")
@@ -22,7 +22,7 @@ class DataRecorder(tk.Tk):
         self.visual_controller = VisualController()
         self.recorder_thread = Thread(target=self.record)
         self.frame_idx = 0
-        self.current_frame = 0
+        self.current_frame_idx = 0
         self.frames = []
         self.controller_states = []
         self.recording = False
@@ -38,15 +38,12 @@ class DataRecorder(tk.Tk):
         self.save_button = tk.Button(self, text="Save", command=self.save_footage)
         self.go_start_button = tk.Button(self, text="Go Start", command=self.go_start)
         self.go_end_button = tk.Button(self, text="Go End", command=self.go_end)
+        self.cut_footage_button = tk.Button(self, text="Cut Footage", command=self.cut_footage)
+        self.start_scale = tk.Scale(self, orient=tk.HORIZONTAL)
+        self.end_scale = tk.Scale(self, orient=tk.HORIZONTAL)
+        self.pause_button.pack()
         self.start_button.pack()
         self.stop_button.pack()
-        self.pause_button.pack()
-        self.next_button.pack()
-        self.prev_button.pack()
-        self.save_file_path_entry.pack()
-        self.save_button.pack()
-        self.go_start_button.pack()
-        self.go_end_button.pack()
 
         self.protocol("WM_DELETE_WINDOW", self.close_app)
 
@@ -62,18 +59,54 @@ class DataRecorder(tk.Tk):
                     frame = self.Mk_screen_capture.capture_frame()
                     self.frames.append(frame)
                     self.controller_states.append(copy.copy(self.controller_state))
-                showen_frame = copy.copy(self.frames[self.current_frame])
-                showen_state = self.controller_states[self.current_frame]
+                showen_frame = copy.copy(self.frames[self.current_frame_idx])
+                showen_state = self.controller_states[self.current_frame_idx]
                 showen_frame = self.visual_controller.draw_controller(showen_frame, showen_state)
                 cv2.imshow("mario kart wii", showen_frame)
 
-                self.current_frame = self.current_frame if self.paused else self.frame_idx
+                self.current_frame_idx = self.current_frame_idx if self.paused else self.frame_idx
                 if not self.paused:
                     self.frame_idx += 1
 
             cv2.destroyAllWindows()
             if not self.app_open:
                 break
+
+    def forget_pause_gui(self):
+        self.next_button.forget()
+        self.prev_button.forget()
+        self.save_file_path_entry.forget()
+        self.save_button.forget()
+        self.go_start_button.forget()
+        self.go_end_button.forget()
+        self.start_scale.forget()
+        self.end_scale.forget()
+        self.cut_footage_button.forget()
+
+    def pack_pause_ui(self):
+        self.next_button.pack()
+        self.prev_button.pack()
+        self.save_file_path_entry.pack()
+        self.save_button.pack()
+        self.go_start_button.pack()
+        self.go_end_button.pack()
+        self.start_scale.pack()
+        self.end_scale.pack()
+        self.cut_footage_button.pack()
+
+    def pack_record_gui(self):
+        self.start_button.pack()
+        self.stop_button.pack()
+
+    def forget_record_gui(self):
+        self.start_button.forget()
+        self.stop_button.forget()
+
+    def update_sliders(self):
+        self.start_scale.config(from_=0, to=self.frame_idx)
+        self.end_scale.config(from_=0, to=self.frame_idx)
+        self.start_scale.set(0)
+        self.end_scale.set(self.frame_idx)
 
     def start_record(self):
         if not self.started:
@@ -84,22 +117,32 @@ class DataRecorder(tk.Tk):
 
     def stop_record(self):
         self.recording = False
+        if self.paused:
+            self.forget_pause_gui()
         self.paused = False
         self.frames.clear()
         self.controller_states.clear()
-        self.frame_idx = self.current_frame = 0
+        self.frame_idx = self.current_frame_idx = 0
 
     def pause_record(self):
         if self.recording:
             self.paused = not self.paused
+            if self.paused:
+                self.update_sliders()
+                self.end_scale.set(self.frame_idx)
+                self.pack_pause_ui()
+                self.forget_record_gui()
+            else:
+                self.forget_pause_gui()
+                self.pack_record_gui()
 
     def prev_frame(self):
-        if self.current_frame > 0:
-            self.current_frame -= 1
+        if self.current_frame_idx > 0:
+            self.current_frame_idx -= 1
 
     def next_frame(self):
-        if self.current_frame < len(self.frames) - 1:
-            self.current_frame += 1
+        if self.current_frame_idx < len(self.frames) - 1:
+            self.current_frame_idx += 1
 
     def handle_inputs(self, input_event):
         if input_event.type == pygame.JOYBUTTONDOWN:
@@ -116,12 +159,20 @@ class DataRecorder(tk.Tk):
             video_writer.write(frame)
 
     def go_start(self):
-        self.current_frame = 0
+        self.current_frame_idx = self.start_scale.get()
 
     def go_end(self):
-        self.current_frame = len(self.frames) - 1
+        self.current_frame_idx = self.end_scale.get()
+
+    def cut_footage(self):
+        self.current_frame_idx = 0
+        self.frames = self.frames[self.start_scale.get():self.end_scale.get()]
+        self.controller_states = self.controller_states[self.start_scale.get():self.end_scale.get()]
+        self.frame_idx = len(self.frames) - 1
+        self.update_sliders()
 
     def close_app(self):
         self.recording = False
+        self.paused = False
         self.app_open = False
         self.destroy()
