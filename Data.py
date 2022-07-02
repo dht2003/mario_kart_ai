70ad5804f8eb9a3d0a49b9532215fe9c26e9529a
dht2003 <44319637+dht2003@users.noreply.github.com>
2022-07-02 17:39:44 +0300

    started working on dataset class

---
+++ Data.py
@@ -0,0 +1,19 @@
+import os
+from cv2 import cv2
+import matplotlib as plt
+import numpy as np
+from torch.utils.data import Dataset, DataLoader
+import torch
+import torchvision
+import math
+
+
+class MKDataSet(Dataset):
+    def __init__(self):
+        pass
+
+    def __getitem__(self, index):
+        pass
+
+    def __len__(self):
+        pass
